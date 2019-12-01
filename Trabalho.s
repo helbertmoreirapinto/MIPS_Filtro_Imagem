c7d4de70d4bb3f91532ca57af807294139949e68
Helbert Moreira Pinto <helbert.tec.info@gmail.com>
2019-12-01 14:35:56 -0300

    HEL_006
    func escreve cabecalho

--- Trabalho.s
+++ Trabalho.s
@@ -2,18 +2,23 @@
 	f_in:		.asciiz "in.pgm"	# File IN
 	f_out:		.asciiz "out.pgm"	# File OUT
 	menu_p:		.asciiz "Filtros de Imagem\n[1]Filtro x\n[2]Filtro y\n[3]Filtro z\n[0]Sair\nSelecione: "
-	new_line:	.word 0x0A
-	tamX_pic:	.word 0
-	tamY_pic:	.word 0
+	cab:		.asciiz "P2\n# Make by Antonio Sebastian / Helbert Pinto #\n"
+	new_line:	.asciiz "\n"
+	c_space:	.asciiz " "
+
 	val_byte:	.space 0x04
 	temp:		.space 0x01
-	buffer:		.space 0x00019000	#100kBytes -> 100 * 1024 = 102400
+		
+	tamPicX:	.word 0
+	tamPicY:	.word 0
+	bytesPic:	.word 0
+	max_value:	.word 255
+	buffer:		.word 0
 
 .text
 .globl main
 
 main:
-
 	# Open File IN [Read]
 	li $v0 0x0D	# system call for open file
 	la $a0 f_in	# input file name
@@ -23,7 +28,7 @@ main:
 	move $k0 $v0	# K0 -> File IN
 	
 	# Check sucess open File IN
-	beq $s0 0xFFFFFFFF END_PROGRAM
+	beq $k0 0xFFFFFFFF END_PROGRAM
 	
 	# Open File OUT [Write]
 	li $v0 0x0D	# system call for open file
@@ -33,24 +38,24 @@ main:
 	syscall		# open file
 	move $k1 $v0	# K1 -> File OUT
 	
-	beq $s1 0xFFFFFFFF END_PROGRAM
+	beq $k1 0xFFFFFFFF END_PROGRAM
 	
 	# Step header File IN
-	lw $t0 new_line
+	lb $t0 new_line
 	li $t3 0x00
-LOOP_ENTER:
+STEP_HREADER:
 	li $v0 0x0E
 	move $a0 $k0
-	la $a1 temp
+	la $a1 temp			# Guarda temporariamente em temp o valor do current char
 	li $a2 0x01
-	syscall
-	lw $t1 temp
-	bne $t0 $t1 LOOP_ENTER	# Check if char is '\n'
-	addi $t3 0x01			# Inc cont '\n'
-	bne $t3 0x02 LOOP_ENTER	# Step 02 '\n'
+	syscall				# Le um char
+	lb $t1 temp			# Salva o char em t1
+	bne $t0 $t1 STEP_HREADER	# Check t0['\n'] != t1[current char]
+	addi $t3 $t3 0x01		# Incr cont '\n'
+	bne $t3 0x02 STEP_HREADER	# Pular 02 '\n'
 
 	# Save X and Save Y
-	li $t0 0x20		# Space 0x20 32 ' '
+	lb $t0 c_space	# Space 0x20 32 ' '
 	li $t3 0x00
 SAVE_RANGE:
 	la $t4 val_byte
@@ -60,44 +65,55 @@ LOOP_RANGE:
 	la $a1 temp		# address of buffer from which to read
 	li $a2 0x01		# read 01 byte (char)
 	syscall			# read file
-	lw $t1 temp
+	lb $t1 temp
 	beq $t0 $t1 SAVE_X	# Check if char is space
 	sb $t1 ($t4)
-	addi $t4 0x01
+	addi $t4 $t4 0x01
 	j LOOP_RANGE
 
 SAVE_X:
-	addi $t3 0x01
+	addi $t3 $t3 0x01
 	beq $t3 0x02 SAVE_Y
-	lw $t0 new_line
+	lb $t0 new_line
 	# x <- val_byte
 	la $a0 val_byte
 	jal STRING_TO_INT
-	sw $v0 tamX_pic
+	sw $v0 tamPicX
 	j SAVE_RANGE
 	
 SAVE_Y:
 	# y <- val_byte
 	la $a0 val_byte
 	jal STRING_TO_INT
-	sw $v0 tamY_pic
+	sw $v0 tamPicY
 
-	lw $t0 new_line
+	lb $t0 new_line
 LOOP_ENTER_2:
 	li $v0 0x0E
 	move $a0 $k0
 	la $a1 temp
 	li $a2 0x01
 	syscall
-	lw $t1 temp
+	lb $t1 temp
 	bne $t0 $t1 LOOP_ENTER_2
-
+	
+	lw $t0 tamPicX
+	lw $t2 tamPicY
+	mulu $t1 $t0 $t2
+	sw $t1 bytesPic
+	li $t0 0x00
+	la $t2 buffer
+INICIAR_BUFFER:
+	sw $zero ($t2)
+	addi $t2 $t2 0x04
+	addi $t0 $t0 0x01
+	bne $t0 $t1 INICIAR_BUFFER
 	# Buffer
-	li $v0 0x0E	# system call for reading from file
-	move $a0 $k0	# file descriptor
-	la $a1 buffer   # address of buffer from which to read
-	li $a2 0x19000	# hardcoded buffer length
-	syscall
+	#li $v0 0x0E	# system call for reading from file
+	#move $a0 $k0	# file descriptor
+	#la $a1 buffer   # address of buffer from which to read
+	#li $a2 0x19000	# hardcoded buffer length
+	#syscall
 	
 	# MENU
 	# Print menu_p in console
@@ -118,11 +134,22 @@ LOOP_ENTER_2:
 FILTRO_1:
 	# Filter 1 - x
 	# Write File OUT
-    move $a0 $k1	# Syscall 15 requieres file descriptor in $a0
-    li $v0 0x0F
-    la $a1 teste
-    la $a2 0x01
-    syscall
+	
+	addi $sp $sp -0x0C
+	lw $a3 tamPicX
+	sw $a3 0($sp)
+	lw $a3 tamPicY
+	sw $a3 4($sp)
+	lw $a3 max_value
+	sw $a3 8($sp)
+	
+	move $a0 $k1
+	la $a1 cab
+	la $a2 val_byte
+	la $a3 c_space
+	jal ESCREVE_CAB
+    
+	
 	j CLOSE_FILE
 	
 FILTRO_2:
@@ -164,34 +191,34 @@ POS_CHAR:
 	beq $s4 0x01 EXIT_LOOP
 	slt $s4 $s7 $s5 	# Check > '9'
 	beq $s4 0x01 EXIT_LOOP
-	addi $s0 0x01	# cont caracteres string
-	addi $s1 0x01
+	addi $s0 $s0 0x01	# cont caracteres string
+	addi $s1 $s1 0x01
 	j POS_CHAR
 	
 EXIT_LOOP:
 	move $s1 $a0
 	lb $s2 ($s1)
-	addi $s2 0xFFFFFFD0
-	addi $s0 0xFFFFFFFF
+	addi $s2 $s2 0xFFFFFFD0
+	addi $s0 $s0 0xFFFFFFFF
 	move $s3 $s0
 	
 CALC:
-	addi $s3 0xFFFFFFFF
+	addi $s3 $s3 0xFFFFFFFF
 	mul $s2 $s2 0x0A
 	bnez $s3 CALC
 	addu $v0 $v0 $s2
-	addi $s0 0xFFFFFFFF
+	addi $s0 $s0 0xFFFFFFFF
 	move $s3 $s0
 	beqz $s3 RET
-	addi $s1 0x01
+	addi $s1 $s1 0x01
 	lb $s2 ($s1)
-	addi $s2 0xFFFFFFD0
+	addi $s2 $s2 0xFFFFFFD0
 	j CALC
 	
 RET:
-	addi $s1 0x01
+	addi $s1 $s1 0x01
 	lb $s2 ($s1)
-	addi $s2 0xFFFFFFD0
+	addi $s2 $s2 0xFFFFFFD0
 	addu $v0 $v0 $s2
 	jr $ra
 
@@ -203,10 +230,12 @@ INT_TO_STRING:
 	
 	li $s0 0x04
 	li $s1 0x00
+	li $s3 0x00
+	
 ZERAR:
 	sb $s1 ($v0)
-	addi $v0 0x01
-	addi $s0 0xFFFFFFFF
+	addi $v0 $v0 0x01
+	addi $s0 $s0 0xFFFFFFFF
 	bnez $s0 ZERAR
 	move $v0 $a0
 	
@@ -214,41 +243,102 @@ ZERAR:
 	li $s0 0x3E8
 	div $s1 $s1 $s0
 	beqz $s1 DIG0_NULL
+	li $s3 0x01
 	move $s2 $s1
-	addi $s1 0x30
+	addi $s1 $s1 0x30
 	sb $s1 ($v0)
-	addi $v0 0x01
-DIG0_NULL:
+	addi $v0 $v0 0x01
 	mul $s1 $s2 0x3E8
 	sub $v1 $v1 $s1
+DIG0_NULL:
 	
 	move $s1 $v1
 	li $s0 0x64
 	div $s1 $s1 $s0
+	bnez $s3 IMP_1
 	beqz $s1 DIG1_NULL
+	li $s3 0x01
+IMP_1:
 	move $s2 $s1
-	addi $s1 0x30
+	addi $s1 $s1 0x30
 	sb $s1 ($v0)
-	addi $v0 0x01
-DIG1_NULL:
+	addi $v0 $v0 0x01
 	mul $s1 $s2 0x64
 	sub $v1 $v1 $s1
+DIG1_NULL:
 	
 	move $s1 $v1
 	li $s0 0x0A
 	div $s1 $s1 $s0
+	bnez $s3 IMP_2
 	beqz $s1 DIG2_NULL
+	li $s3 0x01
+IMP_2:
 	move $s2 $s1
-	addi $s1 0x30
+	addi $s1 $s1 0x30
 	sb $s1 ($v0)
-	addi $v0 0x01
-DIG2_NULL:
+	addi $v0 $v0 0x01
 	mul $s1 $s2 0x0A
 	sub $v1 $v1 $s1
+DIG2_NULL:
 	
 	move $s1 $v1
-	addi $s1 0x30
+	addi $s1 $s1 0x30
 	sb $s1 ($v0)
 	
 	move $v0 $a0
+	jr $ra
+
+#-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-#
+#SUB ROTINA - Escreve cabecalho File OUT
+ESCREVE_CAB:
+	move $s0 $a0	# File OUT
+	move $s1 $a1	# cab
+	move $s2 $a2	# val_byte
+	move $s3 $a3	# c_space
+	lw $s4 0($sp)	# tamX
+	lw $s5 4($sp)	# tamY
+	lw $s6 8($sp)	# max_val
+	addi $sp $sp 0x0C
+	
+	# Write cab in File OUT
+	move $a0 $s0
+    li $v0 0x0F
+   	move $a1 $s1
+	la $a2 0x31
+    syscall
+	
+	move $a0 $s2
+	move $a1 $s4
+	sub $sp $sp 12
+	sw $ra 8($sp)
+	sw $s0 4($sp)
+	sw $s3 0($sp)
+	jal INT_TO_STRING
+	move $s2 $v0
+	lw $s3 0($sp)
+	lw $s0 4($sp)
+	lw $ra 8($sp)
+	add $sp $sp 12
+
+	addi $sp $sp 0x40
+	
+	move $a0 $s0
+    li $v0 0x0F
+    move $a1 $s2
+    la $a2 0x03
+    syscall
+	
+	move $a0 $s0
+    li $v0 0x0F
+    move $a1 $s3
+    la $a2 0x1
+    syscall
+	
+	move $a0 $s0
+    li $v0 0x0F
+    move $a1 $s2
+    la $a2 0x03
+    syscall
+	
 	jr $ra
